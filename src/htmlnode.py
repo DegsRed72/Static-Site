3676746163a10403d6bcb1564867261bc0427983
DegsRed <evanbrokeit@gmail.com>
2025-09-30 14:33:56 -0400

    Added LeafNodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,9 @@
+"""
+tag - A string representing the HTML tag name (e.g. "p", "a", "h1", etc.)
+value - A string representing the value of the HTML tag (e.g. the text inside a paragraph)
+children - A list of HTMLNode objects representing the children of this node
+props - A dictionary of key-value pairs representing the attributes of the HTML tag. For example, a link (<a> tag) might have {"href": "https://www.google.com"}
+"""
 class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -9,8 +15,8 @@ class HTMLNode():
         raise NotImplementedError()
     
     def props_to_html(self):
-        if self.props is None:
-            return None
+        if not self.props:
+            return ""
         message = ""
         for prop in self.props:
             message = message + f' {prop}="{self.props[prop]}"'
@@ -18,3 +24,22 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        html_render = ""
+        if self.value == "" or self.value == None:
+            raise ValueError("All leafnodes must have a value")
+        
+        if self.tag == "" or self.tag == None:
+            return self.value
+        if self.props == None:
+            html_render = f"<{self.tag}>{self.value}</{self.tag}>"
+        else:
+            html_render = f"<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>"
+
+        return html_render
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -27,7 +27,22 @@ class TestHTMLNode(unittest.TestCase):
     def test_no_props(self):
         node =  HTMLNode(None, None, None, None)
         message = node.props_to_html()
-        self.assertEqual(message, None)
+        self.assertEqual(message, "")
 
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_p_with_props(self):
+        node = LeafNode("p", "Hello, world!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<p href="https://www.google.com">Hello, world!</p>')
+
+    def test_leaf_to_html_p_without_value(self):
+        node = LeafNode("p", "")
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_leat_to_html_p_without_tag(self):
+        node = LeafNode("", "Hello, world!")
+        self.assertEqual(node.to_html(), node.value)
 if __name__ == "__main__":
     unittest.main()
